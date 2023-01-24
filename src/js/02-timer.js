6febf094700c1dcb5bf708892d2278bbf1ed4647
NazarSak <112251832+NazarSak@users.noreply.github.com>
2023-01-24 16:28:02 +0200

    4

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,77 +1,98 @@
 import flatpickr from "flatpickr";
-
+import { Notify } from "notiflix/build/notiflix-notify-aio";
 import "flatpickr/dist/flatpickr.min.css";
-
-import Notiflix from 'notiflix';
-
-// flatpickr("#datetime-picker", { options });
-
-
+const   dateInput = document.getElementById("datetime-picker");
+const   startBtn = document.querySelector("button[data-start]");
+const   daysField = document.querySelector("span[data-days]");
+const   hoursField = document.querySelector("span[data-hours]");
+const   minutesField = document.querySelector("span[data-minutes]");
+const   secondsField = document.querySelector("span[data-seconds]");
+const stopBtn = document.createElement("button");
+stopBtn.type = "button";
+stopBtn.textContent = "Stop";
+stopBtn.style.marginLeft = "5px";
+stopBtn.disabled = true;
+startBtn.after(stopBtn);
+startBtn.disabled = true;
 const options = {
-   enableTime: true,
-   time_24hr: true,
-   defaultDate: new Date(),
-   minuteIncrement: 1,
-   onClose(selectedDates) {
-       if (selectedDates[0] < options.defaultDate) {
-         Notify.failure("Please choose a date in the future");
-       } else {
-           options.defaultDate = selectedDates[0];
-           buttonStart.removeAttribute("disabled");
-     }
-   },
- };
-
-
-
-const refs = {
-   generalIn : document.querySelector("input"),
-   startBut : document.querySelector("[data-start]"),
-   timerDiv : document.querySelector(".timer"),
-   fieldDiv : document.querySelector(".field") ,
-   daysEl : document.querySelector("[data-days]"),
-   hoursEl : document.querySelector("[data-hours"),
-   minutesEl : document.querySelector("[data-minutes]"),
-   secondsEl : document.querySelector("data-seconds"),
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose([selectedDates]) {
+    if (isDateCorrect(selectedDates)) {
+      Notify.failure("Please choose a date in the future");
+    }
+    startBtn.disabled = isDateCorrect(selectedDates);
+  },
+};
+let calendar = flatpickr(dateInput, options);
+class Timer {
+  constructor({ updateTimerFace }) {
+    this.intervalId = null;
+    this.isActiv = false;
+    this.updateTimerFace = updateTimerFace;
+  }
+  start() {
+    if (this.isActiv) {
+      Notify.info("Timer already started");
+      return;
+    }
+    this.isActiv = true;
+    stopBtn.disabled = false;
+    const finishTime = calendar.selectedDates[0];
+    this.intervalId = setInterval(() => {
+      const timeForEnd = finishTime - Date.now();
+      const time = this.convertMs(timeForEnd);
+      this.updateTimerFace(time);
+      if (timeForEnd <= 0) {
+        this.stop();
+        Notify.info("Timer is over");
+      }
+    }, 1000);
+  }
+  stop() {
+    if (!this.isActiv) {
+      Notify.info("Timer already stopped");
+      return;
+    }
+    this.isActive = false;
+    clearInterval(this.intervalId);
+    const time = this.convertMs(0);
+    this.updateTimerFace(time);
+  }
+  convertMs(ms) {
+    const second = 1000;
+    const minute = second * 60;
+    const hour = minute * 60;
+    const day = hour * 24;
+    const days = Math.floor(ms / day);
+    const hours = Math.floor((ms % day) / hour);
+    const minutes = Math.floor(((ms % day) % hour) / minute);
+    const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+    return { days, hours, minutes, seconds };
+  }
 }
-
-let selectDate = null;
-let currentDate = null
-
-
-
-refs.startBut.addEventListener("click",submitBut)
-refs.generalIn.addEventListener("input",inputCont)
-
-refs.startBut.setAttribute("disabled",)
-
-
-
-function submitBut (evt) {
-selectDate = new Date(evt.currentTarget.value)
-currentDate = Data.now()
-selectDate - currentDate >= 0 ? refs.startBut.removeAttribute("disabled",) : Notiflix.Notify.failure('Please choose a date in the future')
-
+const timer = new Timer({
+  updateTimerFace,
+});
+function updateTimerFace({ days, hours, minutes, seconds }) {
+  daysField.textContent = addLeadingZero(days);
+  hoursField.textContent = addLeadingZero(hours);
+  minutesField.textContent = addLeadingZero(minutes);
+  secondsField.textContent = addLeadingZero(seconds);
 }
-
-
-function inputCont () {
-setInterval(timer,1000)
-refs.startBut.setAttribute("disabled")
+function addLeadingZero(value) {
+  return String(value).padStart(2, "0");
 }
-
-
-function timer () {
-const diff = selectDate - Data.now()
-
-const days = Math.floor((diff/(1000 * 60 * 60 * 24)));
-const hours = Math.floor((diff/(1000 * 60 * 60)) % 24);
-const minutes = Math.floor((diff/(1000 * 60)) % 60);
-const seconds = Math.floor((diff/(1000)) % 60);
-
-
-refs.daysEl.textContent = days;
-refs.hoursEl.textContent = minutes;
-refs.minutesEl.textContent = seconds;
-refs.secondsEl.textContent = seconds;
+startBtn.addEventListener("click", handleStartBtnClick);
+stopBtn.addEventListener("click", handleStopBtnClick);
+function handleStartBtnClick(event) {
+  timer.start();
+}
+function handleStopBtnClick(event) {
+  timer.stop();
+}
+function isDateCorrect(selectedDates) {
+  return selectedDates < Date.now();
 }
