ce08bdea98f16f12168aaaf5bb7eebcd16ead494
NazarSak <112251832+NazarSak@users.noreply.github.com>
2023-01-24 14:33:21 +0000

    Deploying to gh-pages from @ NazarSak/goit-js-hw-09@63d3a7dfd554e384c673d09ab7a58e7951219f40 🚀

--- 01-color-switcher.25184df5.js
+++
@@ -1,2 +0,0 @@
-!function(){var t=document.querySelector("button[data-start]");console.log(t);var o,e=document.querySelector("button[data-stop]");function n(){var t="#".concat(Math.floor(16777215*Math.random()).toString(16));document.body.style.backgroundColor=t}console.log(e),t.addEventListener("click",(function(){o||(o=setInterval(n,1e3));t.toggleAttribute("disabled")})),e.addEventListener("click",(function(){clearInterval(o),o=null}))}();
-//# sourceMappingURL=01-color-switcher.25184df5.js.map
