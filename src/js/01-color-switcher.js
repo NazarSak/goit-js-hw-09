00e3dfac970f1a563094159f6d961effa091abcb
NazarSak <112251832+NazarSak@users.noreply.github.com>
2023-05-27 12:54:51 +0300

    update

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -36,48 +36,4 @@ function getBgColor() {
 
 
 
-// const start = document.querySelector("button[data-start]");
-// console.log(start);
-
-// const stop = document.querySelector("button[data-stop]");
-// console.log(stop);
-
-// let timeBac;
-
-// // color
-// function getRandomHexColor() {
-//     return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-//   }
-
-// // слухачі 
-// start.addEventListener("click",continueChanging)
-// stop.addEventListener("click",stopInterval)
-
-
-
-// // general
-
-// // const timeBaC = setInterval(bakroundTime,1000)
-
-// // change
-// function bakroundTime () {
-//    const colors = getRandomHexColor()
-//  document.body.style.backgroundColor = colors
-// }
-
-// function continueChanging () {
-//     if (!timeBac) {
-//         timeBac = setInterval(bakroundTime,1000);
-      
-//     }
-//     start.toggleAttribute("disabled"); 
-
-// }
-
-// function stopInterval () {
-//     clearInterval(timeBac);
-//     timeBac = null;
-//     start = null
-// }
-
 
